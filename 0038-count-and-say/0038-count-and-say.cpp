47f861c63b6871e5b5fa71d3ded40baf07812736
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-04-24 21:28:49 +0530

    Time: 7 ms (17.29%), Space: 10.2 MB (37.87%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string say(string s){
+        string res = "";
+        int count=1;
+        for(int i=1;i<s.size();i++)
+        {
+            if(s[i]==s[i-1])//for initial characters, not storing freq, but freq for consecutive same characters
+            {
+                count++;
+            }
+            else
+            {
+                res+=to_string(count)+s[i-1];
+                count=1;
+            }
+        }
+        res+=to_string(count)+s[s.size()-1];//for last character
+        return res;
+    }
+    string countAndSay(int n) {
+    if(n==1) return "1";
+    else
+    {
+        return say(countAndSay(n-1));
+    }   
+    }
+};
