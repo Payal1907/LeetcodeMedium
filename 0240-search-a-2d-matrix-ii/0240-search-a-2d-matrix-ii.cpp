de9e9571c24fbd057072eb57ed5d55f7f3af55c9
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-06-08 18:13:07 +0530

    Time: 44 ms (87.73%), Space: 18.5 MB (91.88%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,7 +3,9 @@ public:
 //isko search matrix 1 ki trh nai kr skte kyuki, first element of each row is not equal to last 
 //element of previous row
 
-//dekh hm shuru krenge last col and first row se, kyuki col aur row dono increasing order me sorted hai, to agr target ki value matrix[row][col] se zyada hai to hm next row pe shift krenge kyuki ek row me last element hi sbse bda hai, otherwise hm ek col kam pe shift krenge
+//dekh hm shuru krenge last col and first row se, kyuki col aur row dono increasing order me sorted
+//hai, to agr target ki value matrix[row][col] se zyada hai to hm next row pe shift krenge kyuki ek
+//row me last element hi sbse bda hai, otherwise hm ek col kam pe shift krenge
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
     int n = matrix.size();
     int m = matrix[0].size();
