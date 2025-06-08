878eb23d26195639ff5a8e3929e117d1304a6148
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-06-08 18:07:19 +0530

    Time: 55 ms (49.41%), Space: 18.6 MB (91.88%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+//isko search matrix 1 ki trh nai kr skte kyuki, first element of each row is not equal to last 
+//element of previous row
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int row = 0, col = m-1;
+    while(row<n && col>=0)
+    {
+        if(matrix[row][col]==target) return true;
+        else if(target>matrix[row][col]) row++; 
+        else col--;
+    }  
+    return false;  
+    }
+};
