1c32d6c112cac8a35e5af9411d661eba7526c7eb
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-06-10 21:23:18 +0530

    Time: 0 ms (100%), Space: 23.6 MB (41.42%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,42 @@
+//we solved using map, it was easy to solve, but what if we want to solve using constant space 
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-    vector<int> ans;
-    int n = nums.size();
-    unordered_map<int,int> mp;
+    int count1 = 0, count2 = 0;
+    int num1 = INT_MIN, num2 = INT_MAX; //we need to get distinct candidates
+//moore's voting used, at max there will be 2 majority elements for this condition
     for(auto num : nums)
     {
-        mp[num]++;
-    } 
-    for(auto pair : mp)  
-    {
-        if(pair.second>std::floor(n/3)) ans.push_back(pair.first);
+//order should be you must compare the num first, then check for count
+        if(num1==num) count1++;
+        else if(num2==num) count2++;
+        else if(count1==0) 
+        {
+            num1 = num;
+            count1 = 1;
+        }
+        else if(count2==0)
+        {
+            num2 = num;
+            count2 = 1;
+        }
+        else
+        {
+            count1--;
+            count2--;
+        }
     }
+    vector<int> ans;
+    int majority = nums.size()/3;
+    count1 = 0, count2 = 0;
+//to recheck if the candidates selected are actually candidates
+    for(auto num : nums)
+    {
+        if(num==num1) count1++;
+        else if(num==num2) count2++;
+    }
+    if(count1>majority) ans.push_back(num1);
+    if(count2>majority) ans.push_back(num2);
     return ans;
     }
 };
