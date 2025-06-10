6e6cf96c44f25fb4f91e30253042247951049949
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-06-10 20:48:36 +0530

    Time: 0 ms (100%), Space: 28.3 MB (23.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,3 +1,9 @@
+//one way is to sort the elements, since every array has a majority element, the majority element
+//occurs more than n/2 times, hence the middlemost element would be majority, since majority
+//must cover more than n/2 indexes
+
+//optimal - we have two variables - candidate and the count
+//count does not actually store the count of candiate but tells that the candidate will retain  a count of > 0 at the end no matter what, count(x)-count(all other elements)>0
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
