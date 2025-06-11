5067c14cd3484c22e202df18a974ce5191e65d8d
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-06-11 21:18:55 +0530

    Time: 44 ms (8.82%), Space: 49.8 MB (7.27%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+
+    bool canPlaceQueen(int row, int col, vector<string> board, int n)
+    {
+        int duplicateRow = row;
+        int duplicateCol = col;
+        while(row>=0 && col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            row--;
+            col--;
+        }
+        row = duplicateRow;
+        col = duplicateCol;
+        while(col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+        row = duplicateRow;
+        col = duplicateCol;
+        while(row<n && col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            row++;
+            col--;
+        }
+
+        return true;
+    }
+
+    void solve(int col, vector<string>& board,vector<vector<string>>& ans,int n)
+    {
+        if(col==n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        for(int row = 0; row < n; row++)
+        {
+            if(canPlaceQueen(row,col,board,n))
+            {
+                board[row][col]='Q';
+                solve(col+1,board,ans,n);
+                board[row][col]='.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> ans;
+    string eachRow(n,'.');
+    vector<string> board(n); //must be of size n, can lead to runtime error
+    for(int i=0;i<n;i++)
+    {
+        board[i]=eachRow;
+    }
+    solve(0,board,ans,n); 
+    return ans;  
+    }
+};
