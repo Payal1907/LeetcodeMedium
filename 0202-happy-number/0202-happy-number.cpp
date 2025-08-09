3d698bd2bd4c7370a47751175f312aeb4219fc4f
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-08-09 21:01:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (67.1%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int count = 10;
+        if(n==1) return true;
+        while(n!=1 && count>0)
+        {
+            int sum=0;
+            int temp=n;
+            while(temp!=0)
+            {
+                int digit=temp%10;
+                sum+=digit*digit;
+                temp=temp/10;
+            }
+            n=sum;
+            if(n==1) return true;
+            count--;
+        }   
+        return false; 
+    }
+};
