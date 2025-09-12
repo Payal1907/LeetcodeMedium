f1e881e0bd460d6e8d9ae6a5e585d68b78667242
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-09-12 11:27:10 +0530

    Time: 3 ms (52.65%), Space: 10.9 MB (75.26%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.cpp
@@ -9,25 +9,24 @@ public:
         int target = sum / 2;
         vector<vector<bool>> t(n + 1, vector<bool>(target + 1, false));
 
-        for (int i = 0; i <= n; i++)
-            t[i][0] = true;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                if (stones[i - 1] <= j)
-                    t[i][j] = t[i - 1][j] || t[i - 1][j - stones[i - 1]];
-                else
-                    t[i][j] = t[i - 1][j];
+        for(int i=0;i<n+1;i++) t[i][0]=true;
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<target+1;j++)
+            {
+                if(stones[i-1]<=j) t[i][j]=t[i-1][j-stones[i-1]]||t[i-1][j];
+                else t[i][j]=t[i-1][j];
             }
         }
-        int closest;
-        for (int j = target; j >= 0; j--) {
-            if (t[n][j]) {
-                closest = j;
+        int closest = 0;
+        for(int i=target;i>=0;i--)
+        {
+            if(t[n][i]) 
+            {
+                closest=i;
                 break;
             }
         }
-
         return sum - 2 * closest;
     }
 };
