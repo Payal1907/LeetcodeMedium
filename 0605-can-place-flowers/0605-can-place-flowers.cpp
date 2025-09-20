24e254a8994242d739a51061079a86f343f9dafb
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-09-20 12:16:06 +0530

    Time: 0 ms (100%), Space: 24.1 MB (74.3%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
     int count = 0;
-    int zeroes = 1;
+    int zeroes = 1; //jo hm ek zeroes count kr rhe the omit krne ke liye agr shur me hua to, isliye 
+    // ek pehle se hi le liye ki sabke liye 2 count kre
     int size = flowerbed.size();
     for(int i=0;i<flowerbed.size();i++)
     {
@@ -14,6 +15,7 @@ public:
         }
     } 
     zeroes++;
+    //similarly jo hm end me ek zero count kr rhe the omit krne ke liye ab hm 2 count kr rhe honge
     count+=(zeroes-1)/2;
     return count>=n;   
     }
