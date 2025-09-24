18782e9a7dc1da8dadb76fb55965ea9b5df64b89
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-09-24 18:31:39 +0530

    Time: 1311 ms (5.04%), Space: 642.2 MB (5.01%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-    string doubled = s+s;
-    string cut = doubled.substr(1,doubled.size()-2);
-    return cut.find(s)!=string::npos;
-    //dekh simple method hai, rotate krne ki bjaye, usko double krke hi aage piche se remove krde ek ek element, agr us doubled-cut me , s present hoga, to true else false
+        int n = s.size();
+        for (int i = 1; i < n; i++) {
+            // rotate by i characters
+            string rotated = s.substr(i) + s.substr(0, i);
+            if (rotated == s)
+                return true;
+        }
+        return false;
     }
 };
