2cfea2b81bd9b834ac9709a09af2d2397997f62c
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-09-30 12:25:56 +0530

    Time: 22 ms (85.93%), Space: 36.4 MB (31.34%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -9,75 +9,81 @@
  */
 class Codec {
 public:
-
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(!root) return "null,";
+        if (!root)
+            return "null,";
         queue<TreeNode*> q;
         q.push(root);
-        string res="";
-        while(!q.empty())
-        {
+        string res = "";
+        while (!q.empty()) {
             int s = q.size();
-            for(int i=0;i<s;i++)
-            {
+            for (int i = 0; i < s; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                if(node)
-                {
-                    res+=to_string(node->val)+",";
+                if (node) {
+                    res += to_string(node->val) + ",";
                     q.push(node->left);
                     q.push(node->right);
-                }
-                else res+="null,";
+                } else
+                    res += "null,";
             }
         }
-        if (!res.empty() && res.back() == ',') res.pop_back();
-        return res;    
+        if (!res.empty() && res.back() == ',')
+            res.pop_back();
+        return res;
     }
-    vector<string> split(string data, char delimiter)
-    {
-        vector<string> nodes;
-        stringstream ss(data);
-        string token;
-        while(getline(ss,token,delimiter))
-        {
-            if(!token.empty()) nodes.push_back(token); 
-            //condition lgaya kyuki stoi will not be able to convert empty string
-        }
-        return nodes;
-    }
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        if(data.empty() || data=="null"||data=="null,") return nullptr;
-        vector<string> nodes = split(data,',');
-        TreeNode* root = new TreeNode(stoi(nodes[0]));
-        queue<TreeNode*> q;
-        q.push(root);
-        int i = 1;
-        while(!q.empty() && i<nodes.size())
-        {
-            TreeNode* curr = q.front();
-            q.pop();
+    vector<string> split(const string& s, char delim) {
+        vector<string> tokens;
+        int n = s.size();
+        int start = 0;
 
-            //left child dhoondhenge
-            if(i<nodes.size() && nodes[i]!="null")
-            {
-                curr->left=new TreeNode(stoi(nodes[i]));
-                q.push(curr->left);
+        for (int i = 0; i < n; i++) {
+            if (s[i] == delim) {
+                if (i > start)
+                    tokens.push_back(s.substr(start, i - start));
+                start = i + 1;
             }
-            i++;
-            if(i<nodes.size() && nodes[i]!="null")
-            {
-                curr->right=new TreeNode(stoi(nodes[i]));
-                q.push(curr->right);
-            }
-            i++;
         }
-        return root;
-           
+
+        if (start < n)
+            tokens.push_back(s.substr(start));
+        else if (start == n)
+            tokens.push_back("");
+
+        return tokens;
     }
-};
+// Decodes your encoded data to tree.
+TreeNode*
+deserialize(string data) {
+    if (data.empty() || data == "null" || data == "null,")
+        return nullptr;
+    vector<string> nodes = split(data, ',');
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        // left child dhoondhenge
+        if (i < nodes.size() && nodes[i] != "null") {
+            curr->left = new TreeNode(stoi(nodes[i]));
+            q.push(curr->left);
+        }
+        i++;
+        // right child dhoondhenge
+        if (i < nodes.size() && nodes[i] != "null") {
+            curr->right = new TreeNode(stoi(nodes[i]));
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+}
+;
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser, deser;
