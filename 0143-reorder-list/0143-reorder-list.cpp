13ef0aaae5fec93bcdb4e399668e6edb9587c750
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-10-01 11:07:36 +0530

    Time: 0 ms (100%), Space: 23.8 MB (19.29%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -12,36 +12,23 @@ class Solution {
 public:
     void reorderList(ListNode* head) {
     if(!head||!head->next||!head->next->next) return;
-    ListNode* fast=head;
-    ListNode* slow=head;
-    //pehle hm middle element dhundhenge
-    while(fast&&fast->next)
+    stack<ListNode*> st;
+    ListNode* curr = head;
+    while(curr)
     {
-        slow=slow->next;
-        fast=fast->next->next;
-    } 
-    //second half ko reverse krde
-    ListNode* prev=nullptr;
-    ListNode* ptr=slow->next;
-    while(ptr)
-    {
-        ListNode* nxt = ptr->next;
-        ptr->next=prev;
-        prev=ptr;
-        ptr=nxt;
+        st.push(curr);
+        curr=curr->next;
     }
-    slow->next=nullptr; //cut both halves
-    //now merging both of them
-    ListNode* first=head;
-    ListNode* second=prev;
-    while(second)
+    int n = st.size();
+    curr=head;
+    for(int i=0;i<n/2;i++)
     {
-        ListNode* t1=first->next;
-        ListNode* t2=second->next;
-        first->next=second;
-        second->next=t1;
-        first=t1;
-        second=t2;
+        ListNode* node = st.top();
+        st.pop();
+        node->next=curr->next;
+        curr->next=node;
+        curr=node->next;
     }
+    curr->next=nullptr;
     }
 };
