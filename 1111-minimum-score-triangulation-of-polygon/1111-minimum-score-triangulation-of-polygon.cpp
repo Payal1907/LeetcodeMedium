f8eecf8c676b65a91cd2bc8cfe5ff4fd9ee1fede
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-10-01 19:23:54 +0530

    Time: 4 ms (31.1%), Space: 10.3 MB (99.4%) - LeetHub

---
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    //variables are i and j
+    int t[51][51];
+    int solve(vector<int>& nums,int i,int j)
+    {
+        if(j-i+1<3) return 0; //min teen sides chahiye
+        if(t[i][j]!=-1) return t[i][j];
+        int res = INT_MAX;
+        for(int k=i+1;k<j;k++)
+        {
+            int wt = nums[i]*nums[j]*nums[k]+solve(nums,i,k)+solve(nums,k,j);
+            res=min(res,wt);
+        }
+        //return t[i][j]=res;
+        t[i][j]=res;
+        return res;
+        //OR
+        //t[i][j]=res
+        //return t[i][j] OR return res;
+    }
+    int minScoreTriangulation(vector<int>& values) {
+        int n = values.size();
+        memset(t,-1,sizeof(t));
+        return solve(values,0,n-1);   
+    }
+};
