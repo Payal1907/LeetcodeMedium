42de14237040251fc4ecb3d282e9d07b731c6c22
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-10-01 19:45:35 +0530

    Time: 95 ms (23.33%), Space: 88.9 MB (32.2%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+    if(nums.empty()) return 0;
+    unordered_set<int> st(nums.begin(),nums.end());
+    int maxi = 0;
+    for(auto num : nums)
+    { 
+        if(!st.count(num-1)) //usse chota koi element nai hai, ab uske upr se  
+        //check krenge
+        { 
+            int curr=num;
+            int count=1;
+            st.erase(curr);
+            while(st.count(curr+1))
+            {
+                curr++;
+                count++;
+                st.erase(curr);
+            }
+            maxi=max(maxi,count);
+        }
+    }
+    //LOOKS LIKE A O(n^2), but can never be since eery elemnt is checked
+    //less than n times, so O(n) pe atak jata hai
+    return maxi;
+    }
+};
