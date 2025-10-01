49dfd0934d00eec3bc8c939df5c84f906fc9847b
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-10-01 18:31:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (45.09%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -9,6 +9,12 @@ public:
         int rem = numBottles-(numExchange*replace);
         numBottles=rem+replace;
     }  
+    //jaise tu apne hath se math solve krti haina, waise hi krke dekhna
+    //sabse pehle to jitni bottle hai utne baar to drink kregi hi
+    //phir tu check kregi ki kitne aur bottle replace kr skte hai depending on
+    //numExchange ki value
+    //ans me usko add kregi
+    //phir numbottle ho jayenge = remaining+jitne replace kiye
     return ans;  
     }
 };
