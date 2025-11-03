d9c48bb11ad4e0a040ebb84f89ce01ada2527d42
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-11-03 14:56:49 +0530

    Time: 5 ms (42.91%), Space: 99.9 MB (35.11%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
@@ -12,6 +12,12 @@ public:
         }
         else maxi=neededTime[i];
     }
-    return needed;   
+    return needed;  
+    //dekh isme simple cheez hai tune pehle ye check kiya tha ki two consecutive 
+    //elements are equal then chose one with lesser needned time
+    //but what is three consecutive elements are equal
+    //in that case, min one we add to the needed time, but maxi one we store 
+    //it for later comparison, if not consecutively equal, then jis element pe
+    //presently hai that becomes the maxi for late comparison 
     }
 };
