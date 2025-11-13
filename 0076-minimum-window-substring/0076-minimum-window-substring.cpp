d0dcfa5f7234d8df0905c99ba1beaf2b955e96f4
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-11-13 15:57:09 +0530

    Time: 3 ms (81.96%), Space: 11.3 MB (94.17%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> mp(128, 0);
+        for (char c : t) {
+            mp[c]++;
+        }
+
+        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
+        while (end < s.size()) {
+            if (mp[s[end++]]-- > 0) {
+                counter--;
+            }
+            while (counter == 0) {
+                if (end - begin < d) {
+                    head = begin;
+                    d = end - head;
+                }
+                if (mp[s[begin++]]++ == 0) {
+                    counter++;
+                }
+            }
+        }
+        return d == INT_MAX ? "" : s.substr(head, d);
+    }
+};
