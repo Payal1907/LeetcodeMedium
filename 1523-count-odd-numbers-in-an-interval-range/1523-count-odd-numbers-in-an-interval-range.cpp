5dfd5dd1cc43912763545cb61f2af5b578d7e091
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-12-07 16:34:59 +0530

    Time: 2 ms (70.23%), Space: 7.9 MB (3.4%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -6,13 +6,15 @@ public:
     // {
     //     if(i%2!=0) count++;
     // }
-    // return count;  
-    if((high-low+1)%2==0) return (high-low+1)/2;
-    else
-    {
-        if(low%2==0 && high%2==0) return (high-low+1)/2;
-        else return ((high-low+1)/2) + 1;
-    }
+    // return count; 
+    int range = (high-low+1); 
+    // if(range%2==0) return range/2;
+    // else
+    // {
+    //     if(low%2==0 && high%2==0) return range/2;
+    //     else return (range/2) + 1;
+    // }
+    return (range%2==0)?range/2:((low%2==0 && high%2==0)?range/2:range/2+1);
 
     }
 };
