9a3c43b31c76199c4b4cfc44c1968ae54af43301
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-12-09 22:46:30 +0530

    Time: 1096 ms (14.59%), Space: 465.4 MB (8.76%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+    long long count = 0;
+    const long long mod = 1e9+7;
+    int n = nums.size();
+    unordered_map<long long,long long> mpr;
+    unordered_map<long long,long long> mpl;
+    for(auto num : nums) mpr[num]++;
+    for(int i=0;i<n;i++)
+    {
+        int curr = nums[i];
+        long long target = nums[i]*2;
+        mpr[curr]--;
+        count=(count+(mpl[target]*mpr[target])%mod)%mod;
+        mpl[nums[i]]++;
+    }  
+    return (int)count;  
+    }
+};
