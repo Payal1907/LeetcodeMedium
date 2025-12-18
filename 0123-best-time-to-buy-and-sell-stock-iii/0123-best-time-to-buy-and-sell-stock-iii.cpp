503e091e2b85dcef78c2aef33910321022d65292
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-12-19 00:07:32 +0530

    Time: 4 ms (74.6%), Space: 79.3 MB (70.41%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -10,9 +10,9 @@ public:
     for(int i=0;i<n;i++) 
     {
         buy1 = min(buy1,prices[i]);
-        profit1 = max(profit1,prices[i]-buy1);
-        buy2 = min(buy2,prices[i]-profit1);
-        profit2 = max(profit2,prices[i]-buy2);
+        profit1 = max(profit1,prices[i]-buy1); //reinvesting
+        buy2 = min(buy2,prices[i]-profit1); //abhi tk I,II,III ke almost same logic hai
+        profit2 = max(profit2,prices[i]-buy2); //reinvesting
     }
     return profit2;
     }
