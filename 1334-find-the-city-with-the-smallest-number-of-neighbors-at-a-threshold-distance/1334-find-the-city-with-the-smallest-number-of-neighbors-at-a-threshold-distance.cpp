007287c11d45be976c47f4f3139d530c64053682
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2025-12-22 16:16:26 +0530

    Time: 111 ms (5.16%), Space: 16.7 MB (63.11%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+    vector<vector<int>> dist(n,vector<int>(n,10001));
+    vector<int> neighbours(n);
+    for(int i=0;i<n;i++)
+    {
+        dist[i][i]=0;
+    }
+    for(int i=0;i<edges.size();i++)
+    {
+        dist[edges[i][0]][edges[i][1]]=edges[i][2];
+        dist[edges[i][1]][edges[i][0]]=edges[i][2];
+    } 
+    for(int k=0;k<n;k++)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(dist[i][k]!=0 && dist[k][j]!=0)
+                {
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+    }
+    for(int i=0;i<dist.size();i++)
+    {
+        for(int j=0;j<dist.size();j++)
+            {
+                cout<<dist[i][j]<<" ";
+            }
+        cout<<endl;
+    }
+    for(int i=0;i<dist.size();i++)
+    {
+        for(int j=0;j<dist.size();j++)
+        {
+            if(dist[i][j]!=0 && dist[i][j]<=distanceThreshold)
+            {
+                neighbours[i]++;
+            }
+        }
+    } 
+    for(auto num : neighbours) cout<<num<<" ";  
+    int index=0;
+    int mini = INT_MAX;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(neighbours[i]<mini)
+        {
+            mini=neighbours[i];
+            index=i;
+        }
+    }
+    return index;
+    }
+};
