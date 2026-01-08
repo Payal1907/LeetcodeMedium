71a175e982c746c919775c9c698d3b879fb931a5
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-01-08 21:08:18 +0530

    Time: 15 ms (76%), Space: 17.8 MB (54.8%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> tt;
-    int solve(int i, int j, vector<int>& nums1, vector<int>& nums2){
-        if(i>=nums1.size() || j>=nums2.size()) return -1e9;
-        if(tt[i][j]!=-1) return tt[i][j];
-        int ans = INT_MIN;
-        ans = max(ans,nums1[i]*nums2[j]+max(0,solve(i+1,j+1,nums1,nums2)));
-        ans = max(ans, solve(i+1,j,nums1,nums2));
-        ans = max(ans,solve(i,j+1,nums1,nums2));
-        return tt[i][j]=ans;
-    }
+    // vector<vector<int>> tt;
+    // int solve(int i, int j, vector<int>& nums1, vector<int>& nums2){
+    //     if(i>=nums1.size() || j>=nums2.size()) return -1e9;
+    //     if(tt[i][j]!=-1) return tt[i][j];
+    //     int ans = INT_MIN;
+    //     ans = max(ans,nums1[i]*nums2[j]+max(0,solve(i+1,j+1,nums1,nums2)));
+    //     ans = max(ans, solve(i+1,j,nums1,nums2));
+    //     ans = max(ans,solve(i,j+1,nums1,nums2));
+    //     return tt[i][j]=ans;
+    // }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        tt.assign(nums1.size(), vector<int>(nums2.size(), -1));
-        return solve(0,0,nums1,nums2);
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> tt;
+        tt.assign(n+1, vector<int>(m+1, -1e9));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int take = nums1[i]*nums2[j]+max(0,tt[i+1][j+1]);
+                int skip1 = tt[i+1][j];
+                int skip2 = tt[i][j+1];
+                tt[i][j]=max({take,skip1,skip2});
+            }
+        }
+        // return solve(0,0,nums1,nums2);
+        return tt[0][0];
     }
 };
