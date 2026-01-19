de679eb2accdafed72d646995d21f83bd7784d7e
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-01-19 21:58:01 +0530

    Time: 138 ms (14.87%), Space: 124.6 MB (11.97%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -5,11 +5,6 @@ public:
     int n = position.size();
     int fleet = 0;
     double pre = 0.0;
-    //last se jo second hai wo last se first wale se uski speed agr zyada hogi aur wo
-    //target pohochne se pehle use join krlega tbhi fleet bnegi
-    //to pehle atleast jo position hai wo minus krle curr position se
-    //aur time nikal liya to aur easy, agr n-1 wale ke paas n-2 se zyada time hua
-    //tabhi n-2 usko join kr payega;
     vector<vector<double>> rem(n,vector<double>(2));
     for(int i=0;i<n;i++){
         rem[i][0]=position[i];
