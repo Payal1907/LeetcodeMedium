7f5e1eb12a311fb28e606a5b892b92f2ffdceef9
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-01-27 21:08:59 +0530

    Time: 361 ms (35.81%), Space: 316.1 MB (44.71%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<pair<int,int>>> graph;
+    int dijkstra(int n) {
+    vector<bool> visited(n,false);
+    vector<int> dist(n,1e9);
+    priority_queue<pair<int,int>> pq;
+    pq.push({0,0});
+    dist[0]=0;
+    while(!pq.empty()){
+        int u = pq.top().second;
+        pq.pop();
+        if(visited[u]) continue;
+        visited[u]=true;
+        for(auto [v,w] : graph[u]){
+            if(dist[u]+w<dist[v]){
+                dist[v]=dist[u]+w;
+                pq.push({-dist[v],v});
+            }
+        }
+    }
+    if(dist[n - 1] == 1e9) return -1;
+    return dist[n-1];
+    }
+    int minCost(int n, vector<vector<int>>& edges){
+        graph.resize(n);
+        for(auto edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            graph[u].push_back({v,w});
+            graph[v].push_back({u,2*w});
+        }
+        return dijkstra(n);
+    }
+};
