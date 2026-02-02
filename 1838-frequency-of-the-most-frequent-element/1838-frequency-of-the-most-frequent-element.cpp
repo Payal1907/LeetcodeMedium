eb837e802f0b2105dea3d743ac2a7ae86c35a1ee
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-02-02 21:42:07 +0530

    Time: 37 ms (65.13%), Space: 106 MB (30.7%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -14,21 +14,6 @@ public:
             }
             ans=max(ans,r-l+1);
         }
-        // vector<int> prefix(n,0);
-        // prefix[0]=nums[0];
-        // for(int i=1;i<n;i++){
-        //     prefix[i]=prefix[i-1]+nums[i];
-        // }
-        // // prefix[n]=prefix[n-1]+nums[n-1];
-        // for(auto pre : prefix) cout<<pre<<" ";
-        // for(int i=n-1;i>=0;i--){
-        //     int expected = nums[i]*(i+1);
-        //     int sum = prefix[i];
-        //     // for(int j=i;j>=0;j--){
-        //     //     sum+=nums[j];
-        //     // }
-        //     if(expected-sum<=k) return i+1;
-        // } 
         return ans;
     }
 };
