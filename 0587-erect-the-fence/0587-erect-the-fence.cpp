2f69dd5e33e27b6c38c0d53e052832c3516b7413
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-02-28 18:19:58 +0530

    Time: 22 ms (29.74%), Space: 27.2 MB (51.31%) - LeetHub

---
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int cross(vector<int>& O, vector<int>& A, vector<int>& B){
+        return (A[0]-O[0])*(B[1]-O[1]) - (A[1]-O[1])*(B[0]-O[0]);
+    }
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+    int n = trees.size();
+    vector<vector<int>> hull;
+    sort(trees.begin(),trees.end());
+    for(int i=0;i<n;i++){
+        while(hull.size()>=2 && cross(hull[hull.size()-2],hull[hull.size()-1],trees[i])<0){
+            hull.pop_back();
+        }
+        hull.push_back(trees[i]);
+    }
+    int t = hull.size()+1;
+    for(int i=n-2;i>=0;i--){
+        while(hull.size()>=t && cross(hull[hull.size()-2],hull[hull.size()-1],trees[i])<0){
+            hull.pop_back();
+        }
+        hull.push_back(trees[i]);
+    } 
+    sort(hull.begin(),hull.end());
+    hull.erase(unique(hull.begin(), hull.end()), hull.end());
+    return hull;  
+    }
+};
