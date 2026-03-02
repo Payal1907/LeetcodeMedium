02bf9068967cd3a921df8dc0ad99b603e772e40b
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-03-02 23:42:06 +0530

    Time: 0 ms (100%), Space: 10.1 MB (34.49%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -5,7 +5,7 @@ public:
         int count=1;
         for(int i=1;i<s.size();i++)
         {
-            if(s[i]==s[i-1])//for initial characters, not storing freq, but freq for consecutive same characters
+            if(s[i]==s[i-1])//for initial characters, not storing freq, but freq for     consecutive same characters
             {
                 count++;
             }
