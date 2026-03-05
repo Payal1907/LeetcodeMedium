c482ed1a0dbab68c822687748bffaef3f6ad81d4
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-03-05 22:42:51 +0530

    Time: 0 ms (100%), Space: 9.6 MB (20.96%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,23 +2,10 @@ class Solution {
 public:
     int minOperations(string s) {
     int n = s.size();
-    string s1,s2;
-    for(int i=0;i<n-1;i+=2){
-        s1+='1';
-        s1+='0';
-        s2+='0';
-        s2+='1';
-    } 
-    if(s1.size()==n-1) s1+='1';
-    if(s2.size()==n-1) s2+='0';
-    int cnt1 = 0;
+    int cnt = 0;
     for(int i=0;i<n;i++){
-        if(s1[i]!=s[i]) cnt1++;
+        cnt+=(s[i]^i)&1;
     }
-    int cnt2 = 0;
-    for(int i=0;i<n;i++){
-        if(s2[i]!=s[i]) cnt2++;
-    } 
-    return min(cnt1,cnt2);
+    return min(cnt,n-cnt); 
     }
 };
