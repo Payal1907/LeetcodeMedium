eb1edf652ee109dcd1aba1ed90b46fdca53ae64c
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-03-05 23:03:30 +0530

    Time: 0 ms (100%), Space: 10.4 MB (81.46%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+      return true; 
+      //alice always wins because she plays first, she can always precompute that
+      //which grp og stones she wants to choose 
+    }
+};
