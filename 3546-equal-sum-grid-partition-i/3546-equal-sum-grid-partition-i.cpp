3a16dd64c138b76752f43ff7e0982ffec77f64c3
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-03-25 10:26:26 +0530

    Time: 7 ms (73.38%), Space: 130.1 MB (53.24%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -3,28 +3,21 @@ public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
     int n = grid.size();
     int m = grid[0].size();
-    if(n==0 || m==0) return false;
-    if(n==1 && m==1) return true;
+    vector<long long> rsum(n,0),csum(m,0);
     long long tsum = 0;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
+            rsum[i]+=grid[i][j];
+            csum[j]+=grid[i][j];
             tsum+=grid[i][j];
         }
     }
-    long long rsum = 0;
-    for(int i=0;i<n-1;i++){
-        rsum+=accumulate(grid[i].begin(),grid[i].end(),0LL);
-        if(tsum-rsum==rsum) return true;
-    } 
-    long long csum = 0;  
-    for(int j=0;j<m-1;j++){
-        long long col = 0;
-        for(int i=0;i<n;i++){
-            col+=grid[i][j];
-        }
-        csum+=col;
-        if(tsum-csum==csum) return true;
-    }
+    for(int i=1;i<n;i++) rsum[i]+=rsum[i-1];
+    for(int i=1;i<m;i++) csum[i]+=csum[i-1];
+
+    for(int i=0;i<n;i++) if(tsum-rsum[i]==rsum[i]) return true;
+    for(int i=0;i<m;i++) if(tsum-csum[i]==csum[i]) return true;
     return false;
+
     }
 };
