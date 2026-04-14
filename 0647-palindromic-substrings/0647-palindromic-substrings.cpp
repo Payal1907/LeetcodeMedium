ebef8e7563cf9816ae4a7d738957a78ad6d2343b
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-04-14 22:34:38 +0530

    Time: 13 ms (45.34%), Space: 21.3 MB (24.22%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    bool isPalindrome(string s){
-        int i=0, j=s.size()-1;
-        while(i<=j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
+    int countPalindrome(string s, int l, int r){
+        int cnt = 0;
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+            cnt++;
+            l--;
+            r++;
         }
-        return true;
+        return cnt;
     }
     int countSubstrings(string s) {
     int ans = 0;
     int n = s.size();
-    for(int i=0;i<n;i++){
-        string ss = "";
-        for(int j=i;j<n;j++){
-            ss+=s[j];
-            if(isPalindrome(ss)) ans++;
-        }
-    }
-    return ans;    
+    for(int i=0;i<n;i++){                          
+        ans+=countPalindrome(s,i,i);
+        // while(l>=0 && r<n && s[l]==s[r]){ //consider every index as centre, find odd length palindrome
+        //     ans++;
+        //     l--;
+        //     r++;
+        // }
+        ans+=countPalindrome(s,i,i+1);
+        // l=i,r=i+1;
+        // while(l>=0 && r<n && s[l]==s[r]){ //even length palindrome
+        //     ans++;
+        //     l--;
+        //     r++;
+        // }
+    }  
+    return ans;  
     }
 };
