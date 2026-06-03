32c730d318f2e3053d98e01f30cbaf1972efb303
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-06-03 09:49:57 +0530

    Time: 0 ms (100%), Space: 27.3 MB (50.74%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    void swap(char* a,char* b)
-    {
-        char temp= *b;
-        *b = *a;
-        *a = temp;
-    }
     void reverseString(vector<char>& s) {
-    int l = 0, r=s.size()-1;  
-    while(l<r)
-    {
-        swap(&s[l++],&s[r--]);
-    } 
+        int n = s.size();
+        for(int i=0;i<n/2;i++){
+            std::swap(s[i],s[n-i-1]);
+        }
     }
 };
