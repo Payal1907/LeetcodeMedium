709729bcefbdc88781943bbb60765ca63e0de6e0
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-06-03 11:06:39 +0530

    Time: 59 ms (19.69%), Space: 44 MB (50%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,29 @@
+class MyCalendar {
+public:
+    set<pair<int,int>> mp;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int startTime, int endTime){
+        bool acc = false;
+        for(auto p : mp){
+            int start = p.first;
+            int end = p.second;
+            if(endTime>start && startTime<end){
+                acc=true;
+            }
+        }
+        if(acc) return false;
+        else{
+            mp.insert({startTime,endTime});
+            return true;
+        }
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
