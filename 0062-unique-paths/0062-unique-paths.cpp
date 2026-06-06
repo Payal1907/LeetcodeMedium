879f9ee05d0ac09d43d10e12305f2bd9a2ab7216
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-06-06 22:19:54 +0530

    Time: 0 ms (100%), Space: 9.4 MB (51.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,16 +7,14 @@ public:
         return dp[i][j] = solve(i+1,j,m,n,dp) + solve(i,j+1,m,n,dp);
     }
     int uniquePaths(int m, int n) {
-    vector<vector<int>> dp(m,vector<int>(n,-1));
-    return solve(0,0,m,n,dp);
-    // for(int i=1;i<m;i++)
-    // {
-    //     for(int j=1;j<n;j++)
-    //     {
-    //         dp[i][j]=dp[i-1][j]+dp[i][j-1]; //row select, col did not select + 
-    //         //col select row did not select
-    //     }
-    // } 
-    // return dp[m-1][n-1];   
+    vector<vector<int>> dp(m+1,vector<int>(n+1,1));
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+    // return solve(0,0,m,n,dp);
+     
     }
 };
