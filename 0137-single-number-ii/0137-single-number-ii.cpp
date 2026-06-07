bc0be69525817691d08f9722d6377a8949e3d0f7
Payal Singh <150468514+Payal1907@users.noreply.github.com>
2026-06-07 11:10:21 +0530

    Time: 0 ms (100%), Space: 13.3 MB (86.47%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,7 +2,16 @@ class Solution {
 public:
 //brute me tu hashmap use krke kr skti hai, lekin TC to linear hogi and SC would also be constant
 //but lets try bitwise 
+    int sortedMethod(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for(int i=0;i<n-1;i+=3){
+            if(nums[i]!=nums[i+1]) return nums[i];
+        }
+        return nums[n-1];
+    }
     int singleNumber(vector<int>& nums) {
+        return sortedMethod(nums);
     int ans =0;
     for(int bitIndex = 0; bitIndex<32; bitIndex++)
     {
